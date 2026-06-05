4a4297bc6784fb0782926c200c871add4260b3e7
HariNandvv <ziafehamin123@gmail.com>
2026-06-05 16:16:03 +0530

    inkwaves
    I changed navbar margin size and footer width

--- inkwaves/src/components/Footer.jsx
+++ inkwaves/src/components/Footer.jsx
@@ -8,16 +8,16 @@ function Footer() {
         <p>© 2026 My Pen Store | All Rights Reserved</p>
         <div className="social-icons">
           <a href="https://facebook.com" target="_blank" rel="noreferrer">
-            <img src="/inkwaves/public/facebook.png" alt="Facebook" />
+            <img src="/facebook.png" alt="Facebook" />
           </a>
           <a href="https://instagram.com" target="_blank" rel="noreferrer">
-            <img src="/inkwaves/public/instagram.png" alt="Instagram" />
+            <img src="/instagram.png" alt="Instagram" />
           </a>
           <a href="https://twitter.com" target="_blank" rel="noreferrer">
-            <img src="/inkwaves/public/twitter.png" alt="Twitter" />
+            <img src="/twitter.png" alt="Twitter" />
           </a>
           <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-            <img src="/inkwaves/public/linkedin.png" alt="LinkedIn" />
+            <img src="/linkedin.png" alt="LinkedIn" />
           </a>
         </div>
       </div>
